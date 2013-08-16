6d9151fe398cb3f6d89494f7a2c653cee657dd1c
Jesus Torres <jtorres0876@gmail.com>
2013-08-16 13:29:32 -0700

    Heroku redirect to root

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     	state = request.env['omniauth.params']['state']
     	user = User.from_omniauth(env["omniauth.auth"])
     	session[:user_id] = user.id
-    	redirect_to discover_path
+    	redirect_to root_url
     end
 
   	def destroy
