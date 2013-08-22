631a36ea38863d043a3114e25351d89e4256c9db
Jesus Torres <jtorres0876@gmail.com>
2013-08-22 15:29:08 -0700

    Facebook hasapp logic complete

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,22 +1,43 @@
 class SharesController < ApplicationController
 	def index
 		@user = current_user
-		@share_list = params[:friend_ids].map(&:to_i)
-		@friends = @user.facebook.get_connection("me", "friends")
+		@friends = @user.facebook.get_connection("me", "friends?fields=id,name,picture,installed")	
+		@share_list = params[:friend_ids]
+		@friendswithapp = []
+		@friendswithoutapp = []
+		@friend = nil
+		@share_list.each do |s|
+			@friend = @friends.detect{|f| f['id'] == s }
+		 	if (@friend['installed'].blank?)
+		 		@friendswithoutapp << @friend['id'].to_i
+		 	else 
+		 		@friendswithapp << @friend['id'].to_i
+		 	end 
+		end
 	end
 
 	def new
 		@user = current_user
-    	@friends = @user.facebook.get_connection("me", "friends")
+    	@friends = @user.facebook.get_connection("me", "friends?fields=id,name,picture,installed")
     	@friendIds = [1]
 	end
 
-	def create	
-		@share_list = params[:friend_ids].map(&:to_i)
-		
+	def create
+		@user = current_user
+		@friends = @user.facebook.get_connection("me", "friends?fields=id,name,picture,installed")	
+		@share_list = params[:friend_ids]
+		@friendswithapp = []
+		@friendswithoutapp = []
+		@friend = nil
+		@share_list.each do |s|
+			@friend = @friends.detect{|f| f['id'] == s }
+		 	if (@friend['installed'].blank?)
+		 		@friendswithoutapp << @friend['id'].to_i
+		 	else 
+		 		@friendswithapp << @friend['id'].to_i
+		 	end 
+		end 
 		@mytext = 'testing text'
-		@friends_with_app = []
-		@friends_without_app = [100005817697208,100006038289344]
 		respond_to do |format|
       		format.html { redirect_to cart_path }
       		format.js
